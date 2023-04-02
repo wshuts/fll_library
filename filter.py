d4f19d11635c04a1af20cec0947eb47b929cb476
wshuts <shutswilliam@gmail.com>
2023-04-01 22:02:56 -0500

    normalize taps and frequency shift (untested)

--- filter.py
+++ filter.py
@@ -1,3 +1,5 @@
+import cmath
+
 from numpy import sinc
 #  port from GNU Radio fll_band_edge_cc_impl
 
@@ -14,15 +16,34 @@ from numpy import sinc
 
 def design_filter(samps_per_sym: float, rolloff: float, filter_size: int):
     m = round(filter_size / samps_per_sym)
-    power = 0
+    power: float = 0
     bb_taps = []
+    d_taps_lower = list(range(filter_size))
+    d_taps_upper = list(range(filter_size))
 
     # Create the baseband filter by adding two sincs together
     tap_index = range(0, filter_size)
     for i in tap_index:
         k = -m + i * 2.0 / samps_per_sym
-        tap = sinc(rolloff * k - 0.5) + sinc(rolloff * k + 0.5)
+        tap: float = sinc(rolloff * k - 0.5) + sinc(rolloff * k + 0.5)
         power += tap
 
         bb_taps.append(tap)
+
+    # Create the band edge filters by spinning the baseband
+    # filter up and down to the right places in frequency.
+    # Also, normalize the power in the filters
+    n: int = (len(bb_taps) - 1.0) / 2.0
+    tap_index = range(0, filter_size)
+    for i in tap_index:
+        tap: float = bb_taps[i] / power
+
+        k: float = (-n + i) / (2.0 * samps_per_sym)
+
+        t1: complex = tap * cmath.exp(-1j * 2 * cmath.pi * (1 + rolloff) * k)
+        t2: complex = tap * cmath.exp(1j * 2 * cmath.pi * (1 + rolloff) * k)
+
+        d_taps_lower[filter_size - i - 1] = t1
+        d_taps_upper[filter_size - i - 1] = t2
+
     return
