6e673ad9fd2609d1b14d4092f4b3e563d946048d
wshuts <shutswilliam@gmail.com>
2023-04-03 22:12:50 -0500

    test symbol times

--- test_band_edge_filter.py
+++ test_band_edge_filter.py
@@ -15,6 +15,18 @@ class TestBandEdgeFilter(unittest.TestCase):
         self.assertEqual(filter_size, 321)
         self.assertEqual(rolloff, 0.5)
 
+    def test_symbol_times(self):
+        band_edge_filter = BandEdgeFilter(16.0, 321, 0.5)
+        band_edge_filter.design()
+
+        newest = band_edge_filter.symbol_times[0]
+        now = band_edge_filter.symbol_times[160]
+        oldest = band_edge_filter.symbol_times[320]
+
+        self.assertEqual(newest, -20.0)
+        self.assertEqual(now, 0.0)
+        self.assertEqual(oldest, 20.0)
+
 
 if __name__ == '__main__':
     unittest.main()
