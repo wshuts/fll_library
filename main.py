46819ee4fa7f0e7218dc719341ddd8be7628b0ab
wshuts <shutswilliam@gmail.com>
2023-04-05 23:25:21 -0500

    script to print the filter tap coefficients

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # This is a sample Python script.
+from band_edge_filter import BandEdgeFilter
 
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -12,7 +13,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    # print_hi('PyCharm')
+    band_edge_filter = BandEdgeFilter(16.0, 321, 0.5)
+    band_edge_filter.design()
+    band_edge_filter.print()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
