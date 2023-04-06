625e67fc6ae733a1c963876a7410d4e94a56951d
wshuts <shutswilliam@gmail.com>
2023-04-05 23:16:47 -0500

    improving readability

--- band_edge_filter.py
+++ band_edge_filter.py
@@ -28,15 +28,15 @@ class BandEdgeFilter:
     def compute_sampling_time(self, index):
         return 2.0 * self.alpha / self.samps_per_sym * (index - self.half_filter_size)
 
-    @staticmethod
-    def compute_tap(sample_time):
-        return np.sinc(sample_time - 0.5) + np.sinc(sample_time + 0.5)
+    def compute_rotation_time(self, index):
+        return 0.5 * (1 + self.alpha) / self.samps_per_sym * (index - self.half_filter_size)
 
     def normalize(self, tap):
         return tap / self.power
 
-    def compute_rotation_time(self, index):
-        return 0.5 * (1 + self.alpha) / self.samps_per_sym * (index - self.half_filter_size)
+    @staticmethod
+    def compute_tap(sample_time):
+        return np.sinc(sample_time - 0.5) + np.sinc(sample_time + 0.5)
 
     @staticmethod
     def compute_angle_lower(rotation_time):
@@ -52,24 +52,55 @@ class BandEdgeFilter:
 
     def design(self):
         tap_index_range = range(0, self.filter_size)
-        self.sampling_times = tuple(self.compute_sampling_time(index) for index in tap_index_range)
-        self.bb_taps = tuple(self.compute_tap(sample_time) for sample_time in self.sampling_times)
+
+        self.sampling_times = tuple(
+            self.compute_sampling_time(index)
+            for index in tap_index_range
+        )
+
+        self.bb_taps = tuple(
+            self.compute_tap(sample_time)
+            for sample_time in self.sampling_times
+        )
         self.power = sum(self.bb_taps)
-        self.bb_taps_normalized = tuple(self.normalize(tap) for tap in self.bb_taps)
+        self.bb_taps_normalized = tuple(
+            self.normalize(tap)
+            for tap in self.bb_taps)
 
-        self.rotation_times = tuple(self.compute_rotation_time(index) for index in tap_index_range)
-        self.angles_lower = tuple(self.compute_angle_lower(rotation_time) for rotation_time in self.rotation_times)
-        self.angles_upper = tuple(self.compute_angle_upper(rotation_time) for rotation_time in self.rotation_times)
+        self.rotation_times = tuple(
+            self.compute_rotation_time(index)
+            for index in tap_index_range
+        )
+        self.angles_lower = tuple(
+            self.compute_angle_lower(rotation_time)
+            for rotation_time in self.rotation_times
+        )
+        self.angles_upper = tuple(
+            self.compute_angle_upper(rotation_time)
+            for rotation_time in self.rotation_times
+        )
 
-        self.tap_angle_pairs_lower = tuple(zip(self.bb_taps_normalized, self.angles_lower))
-        self.taps_lower = tuple(self.compute_rotated_tap(tap, angle)
-                                for (tap, angle) in self.tap_angle_pairs_lower)
-        self.taps_lower_reversed = reversed(self.taps_lower)
+        self.tap_angle_pairs_lower = tuple(
+            zip(self.bb_taps_normalized, self.angles_lower)
+        )
+        self.taps_lower = tuple(
+            self.compute_rotated_tap(tap, angle)
+            for (tap, angle) in self.tap_angle_pairs_lower
+        )
+        self.taps_lower_reversed = tuple(
+            reversed(self.taps_lower)
+        )
 
-        self.tap_angle_pairs_upper = tuple(zip(self.bb_taps_normalized, self.angles_upper))
-        self.taps_upper = tuple(self.compute_rotated_tap(tap, angle)
-                                for (tap, angle) in self.tap_angle_pairs_upper)
-        self.taps_upper_reversed = reversed(self.taps_upper)
+        self.tap_angle_pairs_upper = tuple(
+            zip(self.bb_taps_normalized, self.angles_upper)
+        )
+        self.taps_upper = tuple(
+            self.compute_rotated_tap(tap, angle)
+            for (tap, angle) in self.tap_angle_pairs_upper
+        )
+        self.taps_upper_reversed = tuple(
+            reversed(self.taps_upper)
+        )
         return
 
     def dispose(self):
